138ab52db6ed0886f65982d1d2a765ec77c3ff21
Ayush <rishiustaad@gmail.com>
2024-07-04 17:11:23 +0530

    updated webpark sample login

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Crypto from '../assets/Projects/Crypto.png';
 import Todo from '../assets/Projects/Todo.png';
 import Weather from '../assets/Projects/Weather.png';
@@ -16,7 +15,7 @@ const Work = () => {
           <p className='py-6'>// Check out some of my recent works</p>
         </div>
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          <WorkComp Image={Webpark} description={"E commerce website"} Name={"Web Park"} Demo={'https://e-commece-mern.vercel.app/'} Code={'https://github.com/AyushOJOD/E-Commece-MERN'} SampleEmail={'demo@gmail.com'} SamplePass={'Demo@1234'} />
+          <WorkComp Image={Webpark} description={"E commerce website"} Name={"Web Park"} Demo={'https://e-commece-mern.vercel.app/'} Code={'https://github.com/AyushOJOD/E-Commece-MERN'} SampleEmail={'admin@gmail.com'} SamplePass={'Admin@1234'} />
           <WorkComp Image={Crypto} description={"Real-time crypto website"} Name={"Crypt Crypto"} Demo={'https://crypto-web-app-eight.vercel.app'} Code={'https://github.com/AyushOJOD/Crypto_app_react'} />
           <WorkComp Image={Weather} description={"Real-time weather app"} Name={"Weather App"} Demo={'https://weather-app-nu-sable.vercel.app'} Code={'https://github.com/AyushOJOD/Weather_app_react'} />
           <WorkComp Image={Todo} description={"TODO App"} Name={"TODO Full Stack"} SampleEmail={'Sample@gmail.com'} SamplePass={'Sample@1234'} Demo={'https://todo-mern-vxrj.vercel.app'} Code={'https://github.com/AyushOJOD/todo_MERN'} />
